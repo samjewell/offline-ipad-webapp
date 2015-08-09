5b9a484d9cd3887a12da2c639050ae9b8a116eb4
Sam Jewell <sam.jewell@cantab.net>
2015-08-09 21:06:23 +0100

    show `add to homescreen` more often, for longer but max out at showing it only 5 times.

--- add-to-homescreen/src/addtohomescreen.js
+++ add-to-homescreen/src/addtohomescreen.js
@@ -150,9 +150,9 @@ ath.defaults = {
 	autostart: true,			// show the message automatically
 	skipFirstVisit: false,		// show only to returning visitors (ie: skip the first time you visit)
 	startDelay: 1,				// display the message after that many seconds from page load
-	lifespan: 15,				// life of the message in seconds
-	displayPace: 1440,			// minutes before the message is shown again (0: display every time, default 24 hours)
-	maxDisplayCount: 0,			// absolute maximum number of times the message will be shown to the user (0: no limit)
+	lifespan: 22,				// life of the message in seconds
+	displayPace: 4,			// minutes before the message is shown again (0: display every time, default 24 hours)
+	maxDisplayCount: 5,			// absolute maximum number of times the message will be shown to the user (0: no limit)
 	icon: true,					// add touch icon to the message
 	message: '',				// the message can be customized
 	validLocation: [],			// list of pages where the message will be shown (array of regexes)
